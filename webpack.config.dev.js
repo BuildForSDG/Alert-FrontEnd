2c17b76e363a603886fe23912bbf70a28b1c0f9a
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-17 19:20:51 +0100

    feature - add landing page

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -39,7 +39,12 @@ module.exports = {
       {
         test: /(\.css)$/,
         use: ["style-loader", "css-loader"]
-      }
+      },{
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            'file-loader',
+          ],
+        },
     ]
   }
 };
